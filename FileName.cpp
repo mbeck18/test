be62d15a7a03d515b86b20585881a2e799126e59
mbeck18 <mbeck18@COR240-TL12>
2025-01-16 17:25:09 -0600

    Add project files.

---
+++ FileName.cpp
@@ -0,0 +1,15 @@
+// Name: Manilyn Beck
+// Date: January 16, 2025
+// Program Description:  Github practice
+
+#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+
+	cout << "Hello World" << endl;
+
+}
